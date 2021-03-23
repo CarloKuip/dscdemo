8281e69d38443e37aa07d9122db2c9357a36f3ee
Carlo <carlokuip@gmail.com>
2021-03-23 15:27:07 +0100

    Update democonfig.ps1

--- democonfig.ps1
+++ democonfig.ps1
@@ -3,5 +3,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
-wsl --set-version DistroName 2
+wsl --set-version Ubuntu 2
 wsl --set-default-version 2
