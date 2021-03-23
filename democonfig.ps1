9798ae6d055d7dd280463572b6e133a3eb357a47
Carlo <carlokuip@gmail.com>
2021-03-23 21:18:25 +0100

    Update democonfig.ps1

--- democonfig.ps1
+++ democonfig.ps1
@@ -1,7 +1,1 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
-wsl --set-version Ubuntu 2
-wsl --set-default-version 2
+write-host "hello my friend"
