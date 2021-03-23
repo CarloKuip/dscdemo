e427f70761e4ed6ea6c6c7d74fb24fe1bc594938
Carlo <carlokuip@gmail.com>
2021-03-23 15:26:39 +0100

    Update democonfig.ps1
    added wsl stuff

--- democonfig.ps1
+++ democonfig.ps1
@@ -1,3 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+wsl --set-version DistroName 2
+wsl --set-default-version 2
